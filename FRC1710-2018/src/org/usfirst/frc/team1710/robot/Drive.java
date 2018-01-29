a8ffc960b865e6da2258a192cdd4df5d545d8fe3
AbbyR2020 <33075407+AbbyR2020@users.noreply.github.com>
2018-01-29 17:37:30 -0600

    Made straight driving method and leftDrive and Right Drive

--- FRC1710-2018/src/org/usfirst/frc/team1710/robot/Drive.java
+++ FRC1710-2018/src/org/usfirst/frc/team1710/robot/Drive.java
@@ -9,7 +9,7 @@ public class Drive {
 	public static void arcadeDrive (double forward, double side, boolean shift) {
 		if (shift == true) {
 			//high gear
-			RobotMap.shifter.set(Value.kReverse);
+			RobotMap.shifter.set(Value.kReverse); 
 		} else {
 			//low gear
 			RobotMap.shifter.set(Value.kForward);
@@ -19,4 +19,18 @@ public class Drive {
 		RobotMap.L1.set(ControlMode.PercentOutput, forward + side);
 	}
 	
+	public static void leftDrive(double power) {
+		RobotMap.L1.set(ControlMode.PercentOutput, power);
+	}
+	
+	public static void rightDrive (double power) {
+		RobotMap.R1.set(ControlMode.PercentOutput, power);
+	}
+	
+	public static void straightDrive (double power) {
+		double error = RobotMap.navx.getAngle();
+		double kp= .01;
+		rightDrive(error *kp + power);
+		leftDrive(error*kp - power);
+	}
 }

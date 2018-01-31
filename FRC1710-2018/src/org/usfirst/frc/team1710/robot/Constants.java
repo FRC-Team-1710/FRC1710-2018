b347f971d44cbedf192023b2ea940869497a5476
NathanD15 <33075406+NathanD15@users.noreply.github.com>
2018-01-30 20:05:57 -0600

    merge constants and controllerman into master

---
+++ FRC1710-2018/src/org/usfirst/frc/team1710/robot/Constants.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1710.robot;
+
+public class Constants {
+	
+	public static double distance1 = 0;
+	public static double distance2 = 4000;
+	public static double distance3 = 6000;
+	public static double distance4 = 8000;
+	public static double kPLift = 0.0008;
+	
+	
+}
---
+++ FRC1710-2018/src/org/usfirst/frc/team1710/robot/ControllerMap.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team1710.robot;
+
+public class ControllerMap {
+	
+	static boolean one = RobotMap.mechStick.getRawButton(1);
+	static boolean two = RobotMap.mechStick.getRawButton(2);
+	static boolean three = RobotMap.mechStick.getRawButton(3);
+	static boolean four = RobotMap.mechStick.getRawButton(4);
+	static double liftPower = RobotMap.mechStick.getRawAxis(1);
+
+}

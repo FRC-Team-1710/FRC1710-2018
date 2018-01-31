6ec67d094d945bc5663bdf9ac98157b1c1f79b04
Jake Herman <team1710.cto@gmail.com>
2018-01-30 19:09:38 -0600

    added todo's in lift.java

--- FRC1710-2018/src/org/usfirst/frc/team1710/robot/lift.java
+++ FRC1710-2018/src/org/usfirst/frc/team1710/robot/lift.java
@@ -8,12 +8,15 @@ import edu.wpi.first.wpilibj.AnalogInput;
 public class lift {
 
 	static double height;
+	//make a Constants class that contains all of these setpoints
 	public static double distance1 = 0;
 	public static double distance2 = 4000;
 	public static double distance3 = 6000;
 	public static double distance4 = 8000;
 	public static double setPoint;
 	
+	//TODO: a lot of unnecessary parameters make a ControllerMap class with a bunch of global booleans that are set
+	//equal to buttons from RobotMap.mechStick
 	public static void Lifting(boolean up, boolean down, boolean one,  boolean two,  boolean three, boolean four) {
 	
 		height = RobotMap.lift1.getSelectedSensorPosition(0);
@@ -28,14 +31,15 @@ public class lift {
 			setPoint = distance4;
 		} 
 		 
+		//TODO: instead of calling getRawAxis in here, make double values in ControllerMap that are set equal to these
 		if (RobotMap.mechStick.getRawAxis(1) > 0.2 || RobotMap.mechStick.getRawAxis(1) < -0.2){
+			//so everywhere "RobotMap.mechStick.getRawAxis()" is used, replace it with ControllerMap.liftPower (or whatever you choose to call it)
 			RobotMap.lift1.set(ControlMode.PercentOutput, RobotMap.mechStick.getRawAxis(1));	
 			setPoint = height;
 		}else {	
-			 RobotMap.lift1.set(ControlMode.PercentOutput, (-1 * (((setPoint - height) * .0008))));
+			//TODO: the .0008 should be in the Constants class call it kPLift or something
+			RobotMap.lift1.set(ControlMode.PercentOutput, (-1 * (((setPoint - height) * .0008))));
 		}	
-		 
-
 		 
 		if(RobotMap.liftReset.get() == false) {
 			 RobotMap.lift1.setSelectedSensorPosition(0, 0, 0);	 

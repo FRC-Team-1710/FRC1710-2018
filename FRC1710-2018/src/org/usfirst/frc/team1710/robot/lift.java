828a54a69194a9d4c051b0ba7f0e1cd477ec8596
NathanD15 <33075406+NathanD15@users.noreply.github.com>
2018-01-30 20:03:56 -0600

    New classes for easy editing

--- FRC1710-2018/src/org/usfirst/frc/team1710/robot/lift.java
+++ FRC1710-2018/src/org/usfirst/frc/team1710/robot/lift.java
@@ -4,48 +4,36 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 
-
-public class lift {
-
+public class lift extends Constants{
+	
 	static double height;
-	//make a Constants class that contains all of these setpoints
-	public static double distance1 = 0;
-	public static double distance2 = 4000;
-	public static double distance3 = 6000;
-	public static double distance4 = 8000;
+
 	public static double setPoint;
 	
-	//TODO: a lot of unnecessary parameters make a ControllerMap class with a bunch of global booleans that are set
-	//equal to buttons from RobotMap.mechStick
-	public static void Lifting(boolean up, boolean down, boolean one,  boolean two,  boolean three, boolean four) {
+	public static void Lifting () {
 	
 		height = RobotMap.lift1.getSelectedSensorPosition(0);
 
-		if( one == true) {
+		if( ControllerMap.one == true) {
 			setPoint = distance1;
-		}else if( two == true) {
+		}else if( ControllerMap.two == true) {
 			setPoint = distance2;
-		}else if( three == true) {
+		}else if( ControllerMap.three == true) {
 			setPoint = distance3;
-		}else if(four == true) {
+		}else if(ControllerMap.four == true) {
 			setPoint = distance4;
 		} 
-		 
-		//TODO: instead of calling getRawAxis in here, make double values in ControllerMap that are set equal to these
-		if (RobotMap.mechStick.getRawAxis(1) > 0.2 || RobotMap.mechStick.getRawAxis(1) < -0.2){
-			//so everywhere "RobotMap.mechStick.getRawAxis()" is used, replace it with ControllerMap.liftPower (or whatever you choose to call it)
-			RobotMap.lift1.set(ControlMode.PercentOutput, RobotMap.mechStick.getRawAxis(1));	
+
+		if (ControllerMap.liftPower > 0.2 || ControllerMap.liftPower < -0.2){
+						
+			RobotMap.lift1.set(ControlMode.PercentOutput, ControllerMap.liftPower);	
 			setPoint = height;
-		}else {	
-			//TODO: the .0008 should be in the Constants class call it kPLift or something
-			RobotMap.lift1.set(ControlMode.PercentOutput, (-1 * (((setPoint - height) * .0008))));
+		} else {	
+			RobotMap.lift1.set(ControlMode.PercentOutput, (-1 * (((setPoint - height) * kPLift))));
 		}	
 		 
 		if(RobotMap.liftReset.get() == false) {
-			 RobotMap.lift1.setSelectedSensorPosition(0, 0, 0);	 
+			RobotMap.lift1.setSelectedSensorPosition(0, 0, 0);	 
 		}
-
-
-	}	
-	
+	}		
 }

77bbf121ad89f66d01bd8481c9daabdafcf03645
AbbyR2020 <33075407+AbbyR2020@users.noreply.github.com>
2018-02-02 16:51:49 -0600

    uploaded file of SubSysytemManager

---
+++ FRC1710-2018/src/org/usfirst/frc/team1710/robot/SubsystemManager.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team1710.robot;
+public class SubsystemManager {
+	public static void masterinitilization() {
+		Drive.initilizeDrive();
+		Intake.initilizeIntake();
+		ControllerMap.InitilizeControllerMap();
+		lift.initilizeLift();
+		
+	}
+
+}

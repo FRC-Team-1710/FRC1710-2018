b10d49e491923568ef52b6f75d572cf9b8f0a696
mollymaples <33075400+mollymaples@users.noreply.github.com>
2018-02-05 16:12:41 -0600

    I added some new logic with the current angle and the goal angle

--- FRC1710-2018/src/commands/TurnToAngle.java
+++ FRC1710-2018/src/commands/TurnToAngle.java
@@ -9,32 +9,31 @@ public class TurnToAngle extends Command {
 	
 	double _angle;
 	double _speed;
+	double _goal;
+	double mult = .005;
 	
-    public TurnToAngle(double angle, double speed) {
-
+    public TurnToAngle(double angle) {
     	_angle = angle;
-    	_speed = speed;
-    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	if(RobotMap.navx.getAngle() > _angle) {
-    		Drive.leftDrive(_speed);
+    		Drive.leftDrive((RobotMap.navx.getAngle() - _angle) * mult);
     		
     	}else if(RobotMap.navx.getAngle() < _angle) {
-    		Drive.rightDrive(_speed);
+    		Drive.rightDrive((RobotMap.navx.getAngle() - _angle) * mult);
     		
     	}
     }
-
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return RobotMap.navx.getAngle() < _goal + 5 || RobotMap.navx.getAngle() > _goal - 5;
     }
 
     // Called once after isFinished returns true

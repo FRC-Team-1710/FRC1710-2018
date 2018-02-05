501b2705d12939c9c3d46e7cd9171062b87e7e96
Jake Herman <team1710.cto@gmail.com>
2018-02-05 16:44:20 -0600

    updated turn to angle

--- FRC1710-2018/src/commands/TurnToAngle.java
+++ FRC1710-2018/src/commands/TurnToAngle.java
@@ -2,6 +2,7 @@ package commands;
 
 import org.usfirst.frc.team1710.robot.Drive;
 import org.usfirst.frc.team1710.robot.RobotMap;
+import org.usfirst.frc.team1710.robot.SubsystemManager;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -18,18 +19,12 @@ public class TurnToAngle extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	SubsystemManager.NavxReset();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if(RobotMap.navx.getAngle() > _angle) {
-    		Drive.leftDrive((RobotMap.navx.getAngle() - _angle) * mult);
-    		
-    	}else if(RobotMap.navx.getAngle() < _angle) {
-    		Drive.rightDrive((RobotMap.navx.getAngle() - _angle) * mult);
-    		
-    	}
+    	Drive.arcadeDrive(0, (RobotMap.navx.getAngle() - _angle) * mult, false);
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
@@ -38,6 +33,7 @@ public class TurnToAngle extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Drive.stopDriving();
     }
 
     // Called when another command which requires one or more of the same

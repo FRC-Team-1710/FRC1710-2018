df0f1bab50018678b7e37b047cd79f82bf1db159
Penn Brabec <penn.b@ravenengineer.org>
2018-02-08 16:13:50 -0600

    Added setShifter and RobotMath

---
+++ FRC1710-2018/src/utility/RobotMath.java
@@ -0,0 +1,12 @@
+package utility;
+
+public class RobotMath {
+
+		public static boolean isInRange(double valOne, double valTwo, double distance) {
+			if(valOne >= valTwo - distance && valOne <= valTwo + distance) {
+				return true;
+			}else {
+				return false;
+			}
+		}
+}

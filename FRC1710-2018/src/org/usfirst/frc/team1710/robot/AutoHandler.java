8eb08989ec30fb589f499a392c5c69848dd6c089
Jake Herman <team1710.cto@gmail.com>
2018-03-06 17:38:12 -0600

    in the process of reworking autohandler to use a hashmap to decide what auto to run. It is currently set up to handle all of our one cube autos

--- FRC1710-2018/src/org/usfirst/frc/team1710/robot/AutoHandler.java
+++ FRC1710-2018/src/org/usfirst/frc/team1710/robot/AutoHandler.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team1710.robot;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+
+import java.util.HashMap;
+
 import commandGroups.LeftStartLeftScaleLeftSwitch;
 import commandGroups.LeftStartLeftScaleRightSwitch;
 import commandGroups.LeftStartLeftSwitch;
@@ -17,6 +20,9 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 
 
 public class AutoHandler {
+	
+	static HashMap<String, CommandGroup> autoMap = new HashMap<String, CommandGroup>();
+	
 	/**
 	 * 
 	 * @param switchPos position of the switch, retrieved from FMS 'L' or 'R'
@@ -102,4 +108,43 @@ public class AutoHandler {
     		return null;
     	}
 	}
+	
+	/**
+	 * initializes the hashmap that stores every auto. The key is formatted as follows:
+	 * cube amount, starting position, destination, switch position, scale position
+	 */
+	public static void initAutoMap() {
+		// one cube, starts middle, going to switch, switch and scale on left (scale doesn't matter)
+		autoMap.put("111LL", new MiddleToLeftSwitch());
+		autoMap.put("111LR", new MiddleToRightSwitch());
+		// one cube, starts middle, going to switch, switch and scale on right (scale doesn't matter)
+		autoMap.put("111RR", new MiddleToRightSwitch());
+		autoMap.put("111RL", new MiddleToRightSwitch());
+		// one cube, starts left, going to switch, switch and scale on left (scale doesn't matter)
+		autoMap.put("121LL", new LeftStartLeftSwitch());
+		autoMap.put("121LR", new LeftStartLeftSwitch());
+		// one cube, starts left, going to switch, switch and scale on right (scale doesn't matter)
+		autoMap.put("121RL", new LeftStartRightSwitch());
+		autoMap.put("121RR", new LeftStartRightSwitch());
+		// one cube, starts right, going to switch, switch and scale on left (scale doesn't matter)
+		autoMap.put("131LL", new RightStartLeftSwitch());
+		autoMap.put("131LR", new RightStartLeftSwitch());
+		// one cube, starts right, going to switch, switch and scale on right (scale doesn't matter)
+		autoMap.put("131RL", new RightStartRightSwitch());
+		autoMap.put("131RR", new RightStartRightSwitch());
+		// two cubes, starts left, going to switch and scale, switch and scale on left 
+		autoMap.put("223LL", new LeftStartLeftScaleLeftSwitch());
+		autoMap.put("223LR", new LeftStartLeftScaleLeftSwitch());
+	}
+	
+	public static CommandGroup getAuto(char switchPos, char scalePos, int cubeAmount, int destination, int startPosition) {
+		StringBuilder key = new StringBuilder();
+		key.append(cubeAmount);
+		key.append(startPosition);
+		key.append(destination);
+		key.append(switchPos);
+		key.append(scalePos);
+		return autoMap.get(key);
+	}
+	
 }
